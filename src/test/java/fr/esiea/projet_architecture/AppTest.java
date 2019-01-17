73fc7ac105ec907b3a31f2858275d2d71036487f
Cyril94120 <benamara@et.esiea.fr>
2019-01-17 11:11:20 +0100

    Initialisation Maven

---
+++ src/test/java/fr/esiea/projet_architecture/AppTest.java
@@ -0,0 +1,17 @@
+package fr.esiea.projet_architecture;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    @Test
+    public void testApp()
+    {
+        Assertions.assertThat(true).isTrue();
+    }
+}

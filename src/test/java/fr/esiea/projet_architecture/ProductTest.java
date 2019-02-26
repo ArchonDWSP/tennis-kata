9ed02b1b30aa319c04f3ebd0915a383d520e3985
ZavE <acouturier92@gmail.com>
2019-02-26 16:41:28 +0100

    Code coverage ProductTest for Codecov

--- src/test/java/fr/esiea/projet_architecture/ProductTest.java
+++ src/test/java/fr/esiea/projet_architecture/ProductTest.java
@@ -1,25 +1,35 @@
-package fr.esiea.projet_architecture;
+package esiea.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import fr.esiea.projet_architecture.model.Product;
-import fr.esiea.projet_architecture.model.ProductUnit;
-
 public class ProductTest {
 
 	
-	 	@Test
-	    public void testOnName(){
-	        Product apples = new Product("Pommes", ProductUnit.Kilo);
-	        Assertions.assertThat(apples.getName()).isEqualTo("Pommes");
-	    }
-	 	
-	 	  @Test
-	 	    public void testOnUnit(){
-	 	        Product apples = new Product("Pommes", ProductUnit.Kilo);
-	 	        Assertions.assertThat(apples.getUnit()).isEqualTo(ProductUnit.Kilo);
-	 	    }
-	 	
-	 	
+	@Test
+    public void testEquals () {
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        Discount discount = new Discount(apples, "Apples Discount", 0.199);
+
+        Product banane = new Product("banane", ProductUnit.Kilo);
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        Product apples1 = new Product("apples", ProductUnit.Kilo);
+        Product apples2 = new Product("apples", ProductUnit.Each);
+
+        Assertions.assertThat(apples.equals(apples)).isEqualTo(true);
+        Assertions.assertThat(apples.equals(null)).isEqualTo(false);
+
+        Assertions.assertThat(apples.equals(discount)).isEqualTo(false);
+
+        Assertions.assertThat(apples.equals(banane)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(toothbrush)).isEqualTo(false);
+        
+        Assertions.assertThat(apples.equals(apples1)).isEqualTo(true);
+        Assertions.assertThat(apples.equals(apples2)).isEqualTo(false);
+
+
+    }
+
 }

a26612ebc72271d47344d5b9d34aeae571b5fee0
Bousebta Anes <anes-bousebta@hotmail.com>
2019-02-27 12:22:20 +0100

    bug fixing

--- src/test/java/fr/esiea/projet_architecture/ProductTest.java
+++ src/test/java/fr/esiea/projet_architecture/ProductTest.java
@@ -3,6 +3,10 @@ package fr.esiea.projet_architecture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import fr.esiea.projet_architecture.model.Discount;
+import fr.esiea.projet_architecture.model.Product;
+import fr.esiea.projet_architecture.model.ProductUnit;
+
 public class ProductTest {
 
 	
